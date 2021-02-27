98ab0031c2f9063b6721ce316af574f6a7f98550
Jon Hatfield <johatfie@gmail.com>
2021-02-27 03:31:28 -0500

    More dependencies. RefreshScope

--- src/main/java/com/widget_r_us/gatewayserver/ApiGatewayServerApplication.java
+++ src/main/java/com/widget_r_us/gatewayserver/ApiGatewayServerApplication.java
@@ -3,9 +3,11 @@ package com.widget_r_us.gatewayserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RefreshScope
 public class ApiGatewayServerApplication {
 
 	public static void main(String[] args) {
