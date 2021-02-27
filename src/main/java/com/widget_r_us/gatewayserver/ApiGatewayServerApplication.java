5e4f8b573c7aea4cfbcde87f962413a993f0cb3a
Jon Hatfield <johatfie@gmail.com>
2021-02-27 18:47:31 -0500

    Revise configuration

--- src/main/java/com/widget_r_us/gatewayserver/ApiGatewayServerApplication.java
+++ src/main/java/com/widget_r_us/gatewayserver/ApiGatewayServerApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableEurekaClient
 @RefreshScope
+@EnableRetry
 public class ApiGatewayServerApplication {
 
 	public static void main(String[] args) {
