c93d064ce26d4567a1a44b9f653628ada47663e2
Jon Hatfield <johatfie@gmail.com>
2021-03-04 03:05:26 -0500

    fix package. add logback-spring.xml

--- src/test/java/com/widgets_are_us/gatewayserver/ApiGatewayServerApplicationTests.java
+++ src/test/java/com/widgets_are_us/gatewayserver/ApiGatewayServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.widget_are_us.gatewayserver;
+package com.widgets_are_us.gatewayserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
